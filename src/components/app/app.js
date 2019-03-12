0cfd63051016bee18f129c139958762113554543
xenleme <xenleme@gmail.com>
2019-03-12 07:23:43 +0300

    Added error handling with componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../header';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 import RandomPlanet from '../random-planet';
+import PeoplePage from '../people-page';
+import ErroIndicator from '../error-indicator';
 
 import './app.css';
 
 export default class App extends Component {
   state = {
     showRandomPlanet: true,
-    selectedPerson: 5
+    hasError: false
   };
 
   toggleRandomPlanet = () => {
@@ -20,13 +20,15 @@ export default class App extends Component {
     });
   };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
+  componentDidCatch() {
+    this.setState({ hasError: true });
   };
 
   render() {
+    if (this.state.hasError) {
+      return <ErroIndicator />;
+    }
+
     const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
     return (
@@ -40,14 +42,7 @@ export default class App extends Component {
           Toggle Random Planet
         </button>
   
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   };
--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -89,33 +89,33 @@ const PersonView = ({ person }) => {
           alt="character" />
 
       <div className="card-body">
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                  <span className="term">Gender:</span>
-                  <span>{gender} </span>
-              </li>
-              <li className="list-group-item">
-                  <span className="term">Birth Year:</span>
-                  <span>{birthYear} </span>
-              </li>
-              <li className="list-group-item">
-                  <span className="term">Eye Color:</span>
-                  <span>{eyeColor} </span>
-              </li>
-              <li className="list-group-item">
-                  <span className="term">Hair Color:</span>
-                  <span>{hairColor} </span>
-              </li>
-              <li className="list-group-item">
-                  <span className="term">Height:</span>
-                  <span>{height} </span>
-              </li>
-              <li className="list-group-item">
-                  <span className="term">Mass:</span>
-                  <span>{mass} </span>
-              </li>
-          </ul>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+                <span className="term">Gender:</span>
+                <span>{gender} </span>
+            </li>
+            <li className="list-group-item">
+                <span className="term">Birth Year:</span>
+                <span>{birthYear} </span>
+            </li>
+            <li className="list-group-item">
+                <span className="term">Eye Color:</span>
+                <span>{eyeColor} </span>
+            </li>
+            <li className="list-group-item">
+                <span className="term">Hair Color:</span>
+                <span>{hairColor} </span>
+            </li>
+            <li className="list-group-item">
+                <span className="term">Height:</span>
+                <span>{height} </span>
+            </li>
+            <li className="list-group-item">
+                <span className="term">Mass:</span>
+                <span>{mass} </span>
+            </li>
+        </ul>
       </div>
     </React.Fragment>
   );
