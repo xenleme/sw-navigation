79620f1408230a99b688d777cd48c571863c5cb5
xenleme <xenleme@gmail.com>
2020-05-26 13:18:16 -0700

    Added flex-wrap to the navigation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,20 +5,26 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div className="header d-flex">
+    <div className='header d-flex flex-wrap'>
       <h3>
-        <Link to="/">
-          <img src="https://image.flaticon.com/icons/svg/361/361680.svg" width="50"/>sw navigation</Link>
+        <Link to='/'>
+          <img
+            src='https://image.flaticon.com/icons/svg/361/361680.svg'
+            width='50'
+            alt='Logo'
+          />
+          sw navigation
+        </Link>
       </h3>
-      <ul className="d-flex">
+      <ul className='d-flex flex-wrap'>
         <li>
-          <Link to="/people/">people</Link>
+          <Link to='/people/'>people</Link>
         </li>
         <li>
-          <Link to="/planets/">planets</Link>
+          <Link to='/planets/'>planets</Link>
         </li>
         <li>
-          <Link to="/starships/">starships</Link>
+          <Link to='/starships/'>starships</Link>
         </li>
       </ul>
     </div>
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -13,14 +13,14 @@ export default class RandomPlanet extends Component {
   state = {
     planet: {},
     loading: true,
-    error: false
+    error: false,
   };
 
   componentDidMount() {
     const { updateInterval } = this.props;
     this.updatePlanet();
     this.interval = setInterval(this.updatePlanet, updateInterval);
-  };
+  }
 
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -30,14 +30,14 @@ export default class RandomPlanet extends Component {
     this.setState({
       planet,
       loading: false,
-      error: false
+      error: false,
     });
   };
 
   onError = () => {
     this.setState({
       error: true,
-      loading: false
+      loading: false,
     });
   };
 
@@ -54,12 +54,12 @@ export default class RandomPlanet extends Component {
 
     const hasData = !(loading || error);
 
-    const errorMessage = error ? <ErrorIndicator/> : null;
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = hasData ? <PlanetView planet={planet}/> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
 
     return (
-      <div className="random-planet jumbotron rounded">
+      <div className='random-planet jumbotron rounded'>
         {errorMessage}
         {spinner}
         {content}
@@ -69,34 +69,36 @@ export default class RandomPlanet extends Component {
 }
 
 RandomPlanet.defaultProps = {
-  updateInterval: 3500
-}
+  updateInterval: 3500,
+};
 
 RandomPlanet.propTypes = {
-  updateInterval: PropTypes.number
-}
+  updateInterval: PropTypes.number,
+};
 
 const PlanetView = ({ planet }) => {
   const { id, name, population, rotationPeriod, diameter } = planet;
 
   return (
     <React.Fragment>
-      <img className="planet-image"
-           src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-           alt="Random Planet" />
+      <img
+        className='planet-image'
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+        alt='Random Planet'
+      />
       <div>
         <h4>{name}</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <span className="term">Population:</span>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <span className='term'>Population:</span>
             <span>{population}</span>
           </li>
-          <li className="list-group-item">
-            <span className="term">Rotation Period:</span>
+          <li className='list-group-item'>
+            <span className='term'>Rotation Period:</span>
             <span>{rotationPeriod}</span>
           </li>
-          <li className="list-group-item">
-            <span className="term">Diameter:</span>
+          <li className='list-group-item'>
+            <span className='term'>Diameter:</span>
             <span>{diameter}</span>
           </li>
         </ul>
@@ -104,6 +106,3 @@ const PlanetView = ({ planet }) => {
     </React.Fragment>
   );
 };
-
-
-
